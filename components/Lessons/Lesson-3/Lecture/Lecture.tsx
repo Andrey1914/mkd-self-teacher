c6d91c1f94d95c399f6ca682232cb98e6df8df31
Andrey1914 <anlikajw@gmail.com>
2025-08-13 17:09:14 +0200

    added vocabulary in lesson 3

--- components/Lessons/Lesson-3/Lecture/Lecture.tsx
+++ components/Lessons/Lesson-3/Lecture/Lecture.tsx
@@ -2,25 +2,22 @@ import React from "react";
 
 import { DialogueMeetingWithFriends } from "@/components/Lessons/Lesson-3/Lecture/Dialogue";
 import { PayAttentionBlock } from "@/components/lesson/PayAttentionBlock/PayAttentionBlock";
-// import { Vocabulary } from "@/components/lesson/Vocabulary/Vocabulary";
+import { Vocabulary } from "@/components/lesson/Vocabulary/Vocabulary";
 // import { Paragraph } from "@/components/lesson/Paragraph/Paragraph";
-// import { CountriesNationalitiesTable } from "@/components/lesson/Tables";
 
 import { payAttention1 } from "@/prisma/lessons/pay-attention/lesson-3";
-// import { vocabulary1, vocabulary2 } from "@/prisma/lessons/vocabulary/lesson-1";
+import { vocabulary } from "@/prisma/lessons/vocabulary/lesson-3";
 // import { paragraph } from "@/prisma/lessons/paragraph";
-// import { countriesNationalitiesTable } from "@/prisma/lessons/tables";
 
 const Lecture = () => {
   return (
     <>
       <DialogueMeetingWithFriends />
       <PayAttentionBlock data={payAttention1} />
-      {/* <Vocabulary lesson={vocabulary1} />{" "} */}
+      <Vocabulary lesson={vocabulary} />
       {/* <PayAttentionBlock data={payAttention2} /> */}
       {/* <Vocabulary lesson={vocabulary2} /> */}
       {/* <Paragraph data={paragraph.explanation} /> */}
-      {/* <CountriesNationalitiesTable data={countriesNationalitiesTable} /> */}
     </>
   );
 };
