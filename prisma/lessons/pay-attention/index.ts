860c7bb73019636259489f59ef07c44a2203e5d9
Andrey1914 <anlikajw@gmail.com>
2025-08-26 16:02:38 +0200

    pay attention in lesson 4

--- prisma/lessons/pay-attention/index.ts
+++ prisma/lessons/pay-attention/index.ts
@@ -1,2 +1,3 @@
 export * as payAttentionsLesson1 from "./lesson-1";
 export * as payAttentionsLesson3 from "./lesson-3";
+export * as payAttentionsLesson4 from "./lesson-4";
---
+++ prisma/lessons/pay-attention/lesson-4/index.ts
@@ -0,0 +1,2 @@
+export { payAttention1 } from "./pay-attention-1";
+export { payAttention2 } from "./pay-attention-2";
