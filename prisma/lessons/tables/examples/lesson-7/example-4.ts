49d842aa3528cf18bcfb590bdda2a621bab81d18
Andrey1914 <anlikajw@gmail.com>
2025-10-15 20:25:40 +0200

    examples 1-4, lesson-7

---
+++ prisma/lessons/tables/examples/lesson-7/example-4.ts
@@ -0,0 +1,16 @@
+import { TablesProps } from "@/types";
+
+export const example4: TablesProps = {
+  slug: "lesson-7",
+  type: "example-4",
+
+  content: {
+    text: `В разговорном стиле повторяющийся предлог <span>на</span> может опускаться:`,
+    words: [
+      {
+        mkd: "Им се jа*вувам на И*ван и (на) Мариjа.",
+        ru: "Я звоню Ивану и Марии.",
+      },
+    ],
+  },
+};
