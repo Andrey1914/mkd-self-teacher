e0338a77d1e2f249778bc4eda25b0dd30bce0be6
Andrey1914 <anlikajw@gmail.com>
2025-12-12 19:32:05 +0100

    examples

---
+++ prisma/lessons/tables/examples/lesson-11/example-8.ts
@@ -0,0 +1,17 @@
+import { TablesProps } from "@/types";
+
+export const example8: TablesProps = {
+  slug: "lesson-11",
+  type: "example-8",
+
+  content: {
+    text: `При наличии определённых определений типа <span>о*воj</span>, <span>тоj</span>, <span>о*ноj</span>, <span>наре*дниот</span> 
+    и т.д. предлог <span>во</span> обычно не употребяется:`,
+    words: [
+      {
+        mkd: "Мина*тата го*дина бе*вме во Ки*на.",
+        ru: "В прошлом году мы были в Китае.",
+      },
+    ],
+  },
+};
---
+++ prisma/lessons/tables/examples/lesson-11/index.ts
@@ -0,0 +1,13 @@
+export { example1 } from "./example-1";
+export { example2 } from "./example-2";
+export { example3 } from "./example-3";
+export { example4 } from "./example-4";
+export { example5 } from "./example-5";
+export { example6 } from "./example-6";
+export { example7 } from "./example-7";
+export { example8 } from "./example-8";
+export { example9 } from "./example-9";
+export { example10 } from "./example-10";
+export { example11 } from "./example-11";
+export { example12 } from "./example-12";
+export { example13 } from "./example-13";
