fd2a9de9f26b49b0135a93c12dddc231807ff27f
Andrey1914 <anlikajw@gmail.com>
2026-01-13 21:25:41 +0100

    exercises lesson-13

--- prisma/lessons/exercises/index.ts
+++ prisma/lessons/exercises/index.ts
@@ -10,3 +10,4 @@ export * as exercisesLesson9 from "./lesson-9";
 export * as exercisesLesson10 from "./lesson-10";
 export * as exercisesLesson11 from "./lesson-11";
 export * as exercisesLesson12 from "./lesson-12";
+export * as exercisesLesson13 from "./lesson-13";
